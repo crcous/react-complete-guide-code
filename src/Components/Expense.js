e8490bf9a5541a3837a751a23a60b98133d4cc21
Chase Cousins <100642297+crcous@users.noreply.github.com>
2022-12-28 19:44:25 -0500

    first commit

---
+++ src/Components/Expense.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+import ExpenseFilter from "./ExpenseFilter";
+import ExpensesChart from "./ExpensesChart";
+
+function Expense(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpenseFilter onChangeFilter={filterChangeHandler} />
+        {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+        {filteredExpenses.length > 0 &&
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))}
+        ;
+      </Card>
+    </div>
+  );
+}
+
+export default Expense;
